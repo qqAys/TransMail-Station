087b3a021a7458573cd605a70a4d2c26baf2ea5f
Jinx <me@qqays.xyz>
2025-04-28 11:08:46 +0800

    refactor(upgrade_config): 重构配置升级逻辑
    - 添加获取环境变量版本的函数 get_app_version_form_env
    - 重构配置重写逻辑，使用安全的 yaml.safe_dump 方法
    - 优化代码结构，提高可读性和可维护性

--- upgrade_config.py
+++ upgrade_config.py
@@ -5,12 +5,22 @@ from packaging import version
 
 from app.utils.util import logger
 
+env_version_key = "TRANSMAIL_STATION_VERSION"
+
+
+def get_app_version_form_env():
+    env_value = os.getenv(env_version_key)
+    if env_value is None:
+        EnvironmentError(f"{env_version_key} not set")
+    return env_value
+
 
 class UpgradeConfig:
     config = None
-    should_upgrade = False
+    should_upgrade = None
 
     def __init__(self):
+        self.should_upgrade = False
         logger.info("Checking config.yml ...")
 
         with open("./config/config.yml", "r") as f:
@@ -20,20 +30,25 @@ class UpgradeConfig:
 
         self.upgrade_config(config_version)
 
+        if self.should_upgrade is True:
+            self.rewrite_config()
+        else:
+            logger.info("No need to upgrade config.yml.")
+
     def upgrade_config(self, current_config_version):
+
+        if current_config_version == get_app_version_form_env():
+            return
 
         # config_version is none or less than 0.0.5
         if current_config_version is None or version.parse(
-            current_config_version
+                current_config_version
         ) <= version.parse("0.0.4"):
             logger.info("Upgrading config.yml ...")
             self.should_upgrade = True
 
             # update config version
-            env_version_key = "TRANSMAIL_STATION_VERSION"
-            self.config["app_version"] = os.getenv(env_version_key)
-            if self.config["app_version"] is None:
-                raise EnvironmentError(f"{env_version_key} not set")
+            self.config["app_version"] = get_app_version_form_env()
 
             # update contact_lists, set contact_lists alias using the first mail_server alias name.
             contact_lists = self.config["contact_lists"]
@@ -48,17 +63,25 @@ class UpgradeConfig:
             for email, details in self.config["mail_server"].items():
                 details.pop("alias", None)
 
-        if self.should_upgrade is True:
-            self.rewrite_config()
-        else:
-            logger.info("No need to upgrade config.yml.")
+            return
+
+        if version.parse(current_config_version) < version.parse("0.0.6"):
+            logger.info("Upgrading config.yml ...")
+            self.should_upgrade = True
+
+            # update config version
+            self.config["app_version"] = get_app_version_form_env()
+
+            return
 
     def rewrite_config(self):
         with open("./config/config.yml", "w") as f:
-            yaml.dump(self.config, f)
+            yaml.safe_dump(self.config, f, allow_unicode=True, encoding="utf-8", sort_keys=False, default_flow_style=False)
 
         logger.info(f"Current config version: {self.config['app_version']}")
         logger.info("Rewrite config.yml Done.")
+
+        self.__init__()
 
 
 if __name__ == "__main__":
